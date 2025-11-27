71410430abe1e9836b5218336b3d9e1af6c46483
fakhrany <khaledfakhrany@gmail.com>
2025-11-27 23:25:23 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'cool-ape-red': '#E31E24',
+      },
+    },
+  },
+  plugins: [],
+}
